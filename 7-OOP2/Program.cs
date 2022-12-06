68c1306417e8172ae10a3dba75a9501d74ffc817
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-12-06 18:00:10 +0300

    Добавьте файлы проекта.

---
+++ 7-OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _7_OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new(10, 10);
+            Renderer renderer = new();
+            
+            renderer.DrawCharacter(player.HorizontalCoordinatesCharacter, player.VerticalCoordinatesCharacter);
+        }
+    }
+
+    public class Player
+    {
+        public int VerticalCoordinatesCharacter { get; private set; }
+
+        public int HorizontalCoordinatesCharacter { get; private set; }
+
+        public Player(int verticalCoordinatesCharacter, int horizontalCoordinatesCharacter)
+        {
+            VerticalCoordinatesCharacter = verticalCoordinatesCharacter;
+            HorizontalCoordinatesCharacter = horizontalCoordinatesCharacter;
+        }       
+    }
+
+    public class Renderer
+    {
+        public void DrawCharacter(int verticalCoordinatesCharacter, int horizontalCoordinatesCharacter, char sign = '@')
+        {
+            Console.SetCursorPosition(verticalCoordinatesCharacter, horizontalCoordinatesCharacter);
+            Console.Write(sign);
+        }
+    }
+}
